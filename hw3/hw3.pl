d1ac5b256b0a45190829b8b8dee4c8e6ca8c57f1
pwatter1 <pwatter1@binghamton.edu>
2018-02-06 18:32:05 -0500

    finished with hw3, struggled on binary tree q

--- hw3/hw3.pl
+++ hw3/hw3.pl
@@ -100,14 +100,13 @@ my_member/2.
 my_member(X,[X|_]).
 my_member(X,[_|T]) :- my_member(X,T).
 
-/* 
-http://cs.union.edu/~striegnk/learn-prolog-now/html/node34.html 
-*/ 
+% http://cs.union.edu/~striegnk/learn-prolog-now/html/node34.html 
+ 
 
 /* Problem 1 Test: */
-my_member((1,3),[(1,2),(1,3)]).     % SUCCEED
-my_member(3,[3]).         % SUCCEED
-my_member(4,[1,2,3]).     % FAIL
+:- my_member((1,3),[(1,2),(1,3)]).     % SUCCEED
+:- my_member(3,[3]).         % SUCCEED
+:- my_member(4,[1,2,3]).     % FAIL
 
 /* Problem 2:
  Write a predicate init(All, BLst) that succeeds if Blst has all the items of ALL
@@ -115,14 +114,16 @@ my_member(4,[1,2,3]).     % FAIL
 */
 
 /* Problem 2 Answer: */
-init([_], []).
-init([_|T1], [_|T2]) :- init(T1, T2).
+init([_],[]).
+init([Head1|Tail1], [Head2|Tail2]) :- 
+  Head1 == Head2,
+  init(Tail1, Tail2).
 
 /* Problem 2 Test: */
-% :- init([1], []).       % SUCCEED
-% :- init([1,2,3], [1,2]).% SUCCEED
-% :- init([1,2], [1,2]).  % FAIL
-% :- init([1,2], [2]).	% FAIL
+:- init([1], []).       % SUCCEED
+:- init([1,2,3], [1,2]).% SUCCEED
+:- init([1,2], [1,2]).  % FAIL
+:- init([1,2], [2]).	% FAIL
 
 /* Problem 3:
 Write a predicate is_increasing(X) that succeeds if X is a list of decreasing numbers -- Each number is either the same or lower than the preceding number.
@@ -140,17 +141,17 @@ is_increasing([X, H|T]) :- X =< H, is_increasing([H|T]).
 
 
 /* Problem 3 Test: */
-%:- is_increasing([]).            % SUCCEED
-%:- is_increasing([10]).          % SUCCEED
-%:- is_increasing([9,10]).        % SUCCEED
-%:- is_increasing([7,9,10]).      % SUCCEED
-%:- is_increasing([2,7,7,9,10]).  % SUCCEED
-%:- is_increasing([1,1,1,1,1]).   % SUCCEED
+:- is_increasing([]).            % SUCCEED
+:- is_increasing([10]).          % SUCCEED
+:- is_increasing([9,10]).        % SUCCEED
+:- is_increasing([7,9,10]).      % SUCCEED
+:- is_increasing([2,7,7,9,10]).  % SUCCEED
+:- is_increasing([1,1,1,1,1]).   % SUCCEED
 
-%:- is_increasing([10,9,7,9]).    % FAIL
-%:- is_increasing([2,3,1]).       % FAIL
-%:- is_increasing([3,2,1]).       % FAIL
-%:- is_increasing([77,9]).        % FAIL
+:- is_increasing([10,9,7,9]).    % FAIL
+:- is_increasing([2,3,1]).       % FAIL
+:- is_increasing([3,2,1]).       % FAIL
+:- is_increasing([77,9]).        % FAIL
 
 /* Problem 4:
 Write a predicate element_at(X,Y,N) that succeeds if X is the Nth element of list Y. Y is 0-indexed.
@@ -163,9 +164,9 @@ element_at(X, [H|T], N) :- (N == 0, X == H) ; N1 is N-1, element_at(X,T,N1).
 % https://stackoverflow.com/questions/40964412/how-to-find-nth-element-of-list-and-have-answer-be-in-r-prolog
 
 /* Problem 4 Test: */
-%:- element_at(3,[1,2,3],2).   % SUCCEED
-%:- element_at(1,[1,2,3],0).   % SUCCEED
-%:- element_at(1,[1,2,3],1).   % FAIL
+:- element_at(3,[1,2,3],2).   % SUCCEED
+:- element_at(1,[1,2,3],0).   % SUCCEED
+:- element_at(1,[1,2,3],1).   % FAIL
 
 /* Problem 5:
 Write a predicate insert_at(X,Y,N,Z) that succeeds if Z is the list Y with X inserted at index N -- Insert X at index N in Y.
@@ -177,11 +178,11 @@ insert_at(Elem,List,0,[Elem|List]).
 insert_at(Elem,[E|List],Nth,[E|Result]) :- Nth1 is Nth-1, insert_at(Elem,List,Nth1,Result). 
 
 /* Problem 5 Test: */
-%:- insert_at(3,[1,2,3],2,[1,2,3,3]).  % SUCCEED
-%:- insert_at(1,[1,2,3],0,[1,1,2,3]).  % SUCCEED
-%:- insert_at(a,[1,2,3],1,[1,a,2,3]).  % SUCCEED
+:- insert_at(3,[1,2,3],2,[1,2,3,3]).  % SUCCEED
+:- insert_at(1,[1,2,3],0,[1,1,2,3]).  % SUCCEED
+:- insert_at(a,[1,2,3],1,[1,a,2,3]).  % SUCCEED
 
-%:- insert_at(1,[1,2,3],0,[1,2,3]).    % FAIL
+:- insert_at(1,[1,2,3],0,[1,2,3]).    % FAIL
 
 /* Problem 6:
 Write a predicate zip(Xs,Ys,Zs) that succeeds if Zs is a list where each element is a tuple, (X,Y), with Xs and Ys paired together.
@@ -200,12 +201,12 @@ zip([First|X], [Second|Y], [(First, Second)|Z]) :-
   [Second|Y]), zip(X, Y, Z).
 
 /* Problem 6 Test: */
-%:- zip([1,2,3],[a,b,c],[(1,a),(2,b),(3,c)]). % SUCCEED
-%:- zip([],[],[]).                      % SUCCEED
-%:- zip([1],[2],[(1,2)]).               % SUCCEED
+:- zip([1,2,3],[a,b,c],[(1,a),(2,b),(3,c)]). % SUCCEED
+:- zip([],[],[]).                      % SUCCEED
+:- zip([1],[2],[(1,2)]).               % SUCCEED
 
-%:- zip([1],[2],[(2,3)]).               % FAIL
-%:- zip([1],[2,3],[(1,2)]).             % FAIL
+:- zip([1],[2],[(2,3)]).               % FAIL
+:- zip([1],[2,3],[(1,2)]).             % FAIL
 
 /* Problem 7:
 
@@ -221,13 +222,13 @@ zip2(_, [], []).
 zip2([First|X], [Second|Y], [(First,Second)|Z]) :- zip2(X, Y, Z).
 
 /* Problem 7 Test: */
-%:- zip2([1,2,3],[a,b,c],[(1,a),(2,b),(3,c)]). % SUCCEED
-%:- zip2([],[a,b,c],[]).                  % SUCCEED
-%:- zip2([1,3],[],[]).                    % SUCCEED
-%:- zip2([1,3],[2],[(1,2)]).              % SUCCEED
+:- zip2([1,2,3],[a,b,c],[(1,a),(2,b),(3,c)]). % SUCCEED
+:- zip2([],[a,b,c],[]).                  % SUCCEED
+:- zip2([1,3],[],[]).                    % SUCCEED
+:- zip2([1,3],[2],[(1,2)]).              % SUCCEED
 
-%:- zip2([1],[2],[(2,3)]).                 % FAIL
-%:- zip2([1],[a,b],[(1,a),(1,b)]).         % FAIL
+:- zip2([1],[2],[(2,3)]).                 % FAIL
+:- zip2([1],[a,b],[(1,a),(1,b)]).         % FAIL
 
 /* Problem 8:
 Write a predicate merge(A,B,M) that succeed if the list M has all the items from lists A and B in decreasing order.  Assume that A and B are sorted in decreasing order.  Items do not need to be unique.
@@ -248,11 +249,11 @@ merge([H1|T1], [H2|T2], [H2|R]) :- merge([H1|T1], T2, R).
 % http://www.dailyfreecode.com/code/prolog-merge-two-ordered-list-3129.aspx
 
 /* Problem 8 Test: */
-%:- merge([10,3,2],[11,5,2],[11,10,5,3,2,2]) .       % SUCCEED
-%:- merge([0],[],[0]).                               % SUCCEED
-%:- merge([],[3],[3]).                               % SUCCEED
+:- merge([10,3,2],[11,5,2],[11,10,5,3,2,2]) .       % SUCCEED
+:- merge([0],[],[0]).                               % SUCCEED
+:- merge([],[3],[3]).                               % SUCCEED
 
-%:- merge([4,3],[3],[3]).                            % FAIL
+:- merge([4,3],[3],[3]).                            % FAIL
 
 /* Problem 9:
    See Problem 0B above for the knowledge base used for defining greater_than/2 .
@@ -267,10 +268,8 @@ greater_than(succ(_), 0).
 greater_than(succ(X), succ(Y)):- greater_than(X, Y).
 
 /* Problem 9 Test: */
-% :- greater_than(succ(succ(succ(0))),succ(0)).        % SUCCEED
-% :- greater_than(succ(succ(0)),succ(succ(succ(0)))).  % FAIL
-
-
+:- greater_than(succ(succ(succ(0))),succ(0)).        % SUCCEED
+:- greater_than(succ(succ(0)),succ(succ(succ(0)))).  % FAIL
 
 
 /* Problem 10:
@@ -296,11 +295,11 @@ subtract(X, Y, Z) :- add(Y, Z, X).
 % https://stackoverflow.com/questions/800101/prolog-sum-sub-using-successors
 
 /* Problem 10 Test: */
-% :- subtract(succ(succ(0)), succ(0), succ(0)).       % SUCCEED
-% :- subtract(succ(succ(0)), 0, succ(succ(0))).       % SUCCEED
-% :- subtract(succ(succ(0)), succ(succ(0)), 0).       % SUCCEED
-% :- subtract(succ(succ(0)), 0, 0).	            % FAIL
-% :- subtract(succ(succ(0)), succ(0), succ(succ(0))). % FAIL
+:- subtract(succ(succ(0)), succ(0), succ(0)).       % SUCCEED
+:- subtract(succ(succ(0)), 0, succ(succ(0))).       % SUCCEED
+:- subtract(succ(succ(0)), succ(succ(0)), 0).       % SUCCEED
+:- subtract(succ(succ(0)), 0, 0).	            % FAIL
+:- subtract(succ(succ(0)), succ(0), succ(succ(0))). % FAIL
 
 /* Problem 11:
 (From Learn Prolog NOW!) Binary trees are trees where all internal nodes have exactly two children. The smallest binary trees consist of only one leaf node. We will represent leaf nodes as leaf(Label). For instance, leaf(3) and leaf(7) are leaf nodes, and therefore small binary trees. Given two binary trees B1 and B2 we can combine them into one binary tree using the predicate tree: tree(B1,B2). So, from the leaves leaf(1) and leaf(2) we can build the binary tree tree(leaf(1), leaf(2)). And from the binary trees tree(leaf(1), leaf(2)) and leaf(4) we can build the binary tree tree(tree(leaf(1), leaf(2)), leaf(4)).
@@ -322,15 +321,18 @@ tree(tree(leaf(_), leaf(_)), leaf(_)).
 isBinaryTree(nil).
 isBinaryTree(tree(leaf(X), leaf(Y))) :- tree(leaf(X)), tree(leaf(Y)).
 isBinaryTree(leaf(_)).
-
 isBinaryTree(tree(tree(leaf(_), leaf(_)), leaf(_))).
 isBinaryTree(tree(leaf(_), tree(leaf(_), leaf(_)))).
 
-/* Problem 11 Test: */
-%:- isBinaryTree(leaf(1)).                                           %SUCCEED
-%:- isBinaryTree(tree(leaf(a),leaf(b))).                             %SUCCEED
-%:- BT = tree( leaf(b), tree( leaf(x),leaf(y)) ), isBinaryTree(BT).  %SUCCEED
-%:- BT = tree(tree(leaf(1), leaf(2)), tree(leaf(10), tree(leaf(4), leaf(11)))), isBinaryTree(BT).  %SUCCEED
 
-%:- isBinaryTree( tree(leaf(1)) ).                                   % FAIL
-%:- isBinaryTree( tree() ).                                          % FAIL
+% really struggled with this one
+
+
+/* Problem 11 Test: */
+:- isBinaryTree(leaf(1)).                                           %SUCCEED
+:- isBinaryTree(tree(leaf(a),leaf(b))).                             %SUCCEED
+:- BT = tree( leaf(b), tree( leaf(x),leaf(y)) ), isBinaryTree(BT).  %SUCCEED
+:- BT = tree(tree(leaf(1), leaf(2)), tree(leaf(10), tree(leaf(4), leaf(11)))), isBinaryTree(BT).  %SUCCEED
+
+:- isBinaryTree( tree(leaf(1)) ).                                   % FAIL
+:- isBinaryTree( tree() ).                                          % FAIL
